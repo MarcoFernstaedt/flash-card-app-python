146f8255026f9392575a48e5c7d6a0e4f97098ce
Marco <fernstaedtmarco@gmail.com>
2022-10-18 14:38:32 -0700

    card auto shows english side after 3 seconds

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
+import random
+import pandas
 from tkinter import *
 
-from pygments import highlight
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+def next_word():
+    global current_card
+    current_card = random.choice(to_learn)
+    current_card["French"]
+    canvas.itemconfig(card_word, text=current_card["French"])
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_bg, image=card_back_img)
 
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
+
 # canvas
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img, )
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"), )
-canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+card_back_img = PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=card_front_img, )
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 checkmark_img = PhotoImage(file="images/right.png")
-known_button = Button(image=checkmark_img, highlightthickness=0)
+known_button = Button(image=checkmark_img, highlightthickness=0, command=next_word)
 known_button.grid(column=1, row=1)
 
 cross_img = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_img, highlightthickness=0)
+unknown_button = Button(image=cross_img, highlightthickness=0, command=next_word)
 unknown_button.grid(column=0, row=1)
 
+next_word()
 
 window.mainloop()
